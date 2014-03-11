47286848576d9d905d123c78034a61070908d2d7
jotitan <titanbar@hotmail.com>
2014-03-11 17:34:39 +0100

    First commit, poc canvas

---
+++ js/tichu.js
@@ -0,0 +1,7 @@
+/* TODO : creer le jeu de cartes */
+/* TODO : Creer carte speciale (image) */
+/* TODO : cacher les cartes des autres joueurs */
+/* TODO : gerer un mode recto et un mode verso */
+/* TODO : Case pour l'echange au depart (seul mouvement libre) */
+/* TODO : detecter les bombes */
+/* TODO : validation des regles (par rapport au carte d'avant) */
